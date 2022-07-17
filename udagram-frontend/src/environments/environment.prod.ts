fe0ff2aca44b4fcb11a134de32324fec2d13fe00
unknown <nguyenngochadt99@gmail.com>
2022-07-18 01:16:41 +0700

    change url api

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a62ded206372546c3ae943e39ad4d140-1823818735.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
